d2fecc05e0bd39aada5b10195725ac26253bd1e3
julianruben <julian.ruben@stellantis.com>
2021-05-07 15:28:10 +0000

    Project Code

--- alta3research-pythoncert01.py
+++ alta3research-pythoncert01.py
@@ -7,13 +7,33 @@ This program will cover to,
 4. script with main()"""
 
 import requests
+import os
 
-def main():
+def findPopulation():
     """Main function to retrive the data from Data USA  API"""
     # create request object for Data USA
-    reqNasa = requests.get("https://datausa.io/api/data?drilldowns=Nation&measures=Population")
-    for x in range(len(reqNasa.json().get('data'))):
-        print(f"{x}: {reqNasa.json().get('data')[x].get('Year')}")
-    yearRequest = int(input("Please choose the number from the year list above to find out the population\n>"))
-    print(f"The total population for the year {reqNasa.json().get('data')[yearRequest].get('Year') } is {reqNasa.json().get('data')[yearRequest].get('Population')}")
-main()
+    reqDataUSA = requests.get("https://datausa.io/api/data?drilldowns=Nation&measures=Population")
+    os.system('clear')
+    for x in range(len(reqDataUSA.json().get('data'))):
+        print(f"{x}: {reqDataUSA.json().get('data')[x].get('Year')}")
+    yearRequest = input("Please choose the number from the year list above to find out the population\n>")
+    if yearRequest.isdigit() == True:
+        yearRequest = int(yearRequest)
+        if yearRequest >= 0 and yearRequest < 6:
+            print(f"\nThe total population for the year {reqDataUSA.json().get('data')[yearRequest].get('Year') } is {reqDataUSA.json().get('data')[yearRequest].get('Population')}")
+            print(f"\nThe source of this data is from {reqDataUSA.json().get('source')[0].get('annotations').get('source_name')} - {reqDataUSA.json().get('source')[0].get('annotations').get('source_description')}")
+        else:
+            print("The choice you have enter is not present")
+    else:
+        print("The choice you have enter is not present")
+
+
+def main():
+    toContinue = "Y"
+    while toContinue == "Y":
+        findPopulation()
+        toContinue = input("\nDo you want to continue (Y / N)?").upper()
+    if toContinue.upper() != "N":
+        print("You have not chosen Y / N, hence quiting the program")
+if __name__ == "__main__":
+    main()
